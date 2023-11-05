25d80aaee34e5c0c682668ae2f192b4cd9acced9
Gabriel Teixeira <107567915+G4bi567@users.noreply.github.com>
2023-11-05 13:28:29 +0100

    without collision with object detected

--- sketch.js
+++ sketch.js
@@ -1,75 +1,37 @@
-const { Engine, World, Bodies, Composite } = Matter; // Import required Matter.js modules
+const { Engine, World, Composite, Bodies } = Matter;
 
 let engine;
 let world;
 let boxes = [];
 let ground;
+let sol;
 let player1;
 const respawnPosition = { x: 100, y: 100 };
-let matterEngine; // Create a separate Matter.js engine for events
-let playerContacted = false;
 
 function setup() {
     createCanvas(1024, 576);
 
-    // Initialize the main Matter.js engine for the world physics
     engine = Engine.create();
     world = engine.world;
-    ground = new Boundary(0, height, width, 100);
+    ground = new Boundary(0, 400, 400, 100);
     Composite.add(world, ground);
-    player1 = new Player(100, 100, 100, 100);
+    sol = new Boundary(400, 200, 500, 100);
+    Composite.add(world, sol);
+    console.log(sol)
+    player1 = new Player(100, 100, 50, 100);
     Composite.add(world, player1);
 
-    // Initialize a separate Matter.js engine for events
-    matterEngine = Engine.create();
-    World.add(matterEngine.world, [player1.body, ground.body]);
 
-    // Set up the collision event handling
-    Matter.Events.on(matterEngine, 'collisionStart', (event) => {
-        console.log("Collision event started"); // Add this line
-        const pairs = event.pairs;
-        for (let i = 0; i < pairs.length; i++) {
-            const pair = pairs[i];
-            if (pair.bodyA === player1.body && pair.bodyB.label === "ground") {
-                console.log("Player touched the ground");
-            } else if (pair.bodyA === player1.body && pair.bodyB.label === 'box') {
-                console.log("Player touched a box");
-            }
-        }
-    });
-    
-    
-    Matter.Events.on(matterEngine, 'collisionEnd', (event) => {
-        const pairs = event.pairs;
-        for (let i = 0; i < pairs.length; i++) {
-            const pair = pairs[i];
-            if ((pair.bodyA === player1.body && pair.bodyB === ground.body) || (pair.bodyB === player1.body && pair.bodyA === ground.body)) {
-                playerContacted = false; // Reset the contact state when contact ends
-            }
-        }
-    });
-}
-
-
-function mouseDragged() {
-    boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
-}
-
-function jump() {
-    const force = { x: 0, y: -0.3 }; // Adjust the y value for jump strength
-    Matter.Body.applyForce(player1.body, player1.body.position, force);
 }
 
 const keys = { d: false, a: false };
 
 window.addEventListener("keydown", (event) => {
     if (event.key in keys) {
-        keys[event.key] = true;}
-    else if (event.key === 'w' && Math.abs(player1.body.velocity.y)  < 0.01)  {
-            jump();
-        }
-
-    
+        keys[event.key] = true;
+    } else if (event.key === 'w' && Math.abs(player1.body.velocity.y) < 0.01) {
+        jump();
+    }
 });
 
 window.addEventListener("keyup", (event) => {
@@ -77,7 +39,14 @@ window.addEventListener("keyup", (event) => {
         keys[event.key] = false;
     }
 });
+function mouseDragged() {
+    boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
+}
 
+function jump() {
+    const force = { x: 0, y: -0.3 };
+    Matter.Body.applyForce(player1.body, player1.body.position, force);
+}
 
 function respawnPlayer() {
     Matter.Body.setPosition(player1.body, respawnPosition);
@@ -104,8 +73,6 @@ function draw() {
 
     Matter.Body.setVelocity(player1.body, playerVelocity);
 
-    // Block the player at the world edges
-    // Block the player at the world edges
     if (player1.body.position.x < player1.w / 2) {
         Matter.Body.setPosition(player1.body, { x: player1.w / 2, y: player1.body.position.y });
     } else if (player1.body.position.x > width - player1.w / 2) {
@@ -114,8 +81,8 @@ function draw() {
 
     ground.show();
     player1.show();
+    sol.show()
 
-    // Check if the player has fallen off the map and respawn them
     if (player1.body.position.y > height + 100) {
         respawnPlayer();
     }
