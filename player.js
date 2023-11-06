9828fae214ea2520dbc8733377203daa91cac1b5
Gabriel Teixeira <107567915+G4bi567@users.noreply.github.com>
2023-11-07 00:50:29 +0100

    probleme de run à résoudre

--- player.js
+++ player.js
@@ -4,28 +4,72 @@ class Player {
         this.y = y;
         this.w = w;
         this.h = h;
+        this.zoomX = 6; // Zoom factor for the X dimension
+        this.zoomY = 4; // Zoom factor for the Y dimension
+
         let options = {
             friction: 0,
             inertia: Infinity,
             restitution: 0,
             center: true
-        }
+        };
+
         this.body = Bodies.rectangle(this.x, this.y, this.w, this.h, options);
-        
         Composite.add(world, this.body);
+
+        this.spriteIdle = loadImage('./Sprites/character/Idle.png');
+        this.spriteRun = loadImage('./Sprites/character/Run.png');
+        this.frameWidth = 200;
+        this.frameHeight = 200;
+        this.frames = 8;
+        this.currentFrame = 0;
+
+        this.frameRate = 10; // Set the frame rate (e.g., 10 frames per second)
+        this.frameCounter = 0;
+         // Set the frame rate for the entire sketch
     }
 
     show() {
         let pos = this.body.position;
         let angle = this.body.angle;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rectMode(CENTER);
-        strokeWeight(1);
-        stroke(255)
-        fill("red");
-        rect(0, 0, this.w, this.h);
-        pop();
+        let scaledWidth = this.w * this.zoomX;
+        let scaledHeight = this.h * this.zoomY;
+        console.log(this.body.velocity.y)
+        // Check the player's velocity
+        if (this.body.velocity.y === 0 && this.body.velocity.x > 0) {
+            console.log("run")
+            // If velocity.y is 0 and velocity.x is greater than 0, switch to the running sprite
+            image(
+                this.spriteIdle,
+                -scaledWidth / 2,
+                -scaledHeight / 2,
+                scaledWidth,
+                scaledHeight,
+                this.currentFrame * this.frameWidth,
+                0,
+                this.frameWidth,
+                this.frameHeight
+            );
+        } else {
+            // Otherwise, use the idle sprite
+            image(
+                this.spriteIdle,
+                -scaledWidth / 2,
+                -scaledHeight / 2,
+                scaledWidth,
+                scaledHeight,
+                this.currentFrame * this.frameWidth,
+                0,
+                this.frameWidth,
+                this.frameHeight
+            );
+        }
+
+        // Update the animation frame based on the frame rate
+        this.frameCounter++;
+        if (this.frameCounter >= this.frameRate) {
+            this.currentFrame = (this.currentFrame + 1) % this.frames;
+            this.frameCounter = 0;
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,113 +1,141 @@
 const { Engine, World, Composite, Bodies } = Matter;
-
-let engine;
-let world;
-let boxes = [];
-let ground;
-let sol;
-let player1;
-const respawnPosition = { x: 100, y: 100 };
-let backgroundImage; // Declare a variable to store the image
-let CollisionBlocks=[]
-
-function setup() {
-    createCanvas(560, 1168);
-
-    engine = Engine.create();
-    world = engine.world;
-    ground = new Boundary(0, 400, 400, 100);
-    Composite.add(world, ground);
-    sol = new Boundary(400, 600, 600, 50);
-    Composite.add(world, sol);
-    console.log(sol)
-    player1 = new Player(32, 32, 16, 16);
-    Composite.add(world, player1);
-    var floorCollision2D=[]
-    for(let i = 0;i < floorCollision.length; i+=35 ){
-        floorCollision2D.push(floorCollision.slice(i,i+35))
-    }
-     
-    floorCollision2D.forEach((row,y) => {
-        row.forEach((symbol,x )=>{
-            if (symbol==3193){
-                console.log("hello");
-                CollisionBlocks.push(new Boundary(x*16, y*16, 16, 16));
-            }
-        })
-        
-    });
-    Composite.add(world, CollisionBlocks);
-}
-
-
-
-function preload() {
-    // Load the image before setting up the canvas
-    backgroundImage = loadImage('Sprites/Background/sans titre.png');
-  }
-
+const respawnPosition = { x: 0, y: 1050 };
 const keys = { d: false, a: false };
 
+let engine, world, boxes = [],zoom=2, ground, sol, player1, backgroundImage, CollisionBlocks = [], platformCollisionBlocks = [];
+
+function createCollisionBlocks(data, collisionArray, yOffset, height) {
+    const collision2D = [];
+    for (let i = 0; i < data.length; i += 35) {
+        collision2D.push(data.slice(i, i + 35));
+    }
+    collision2D.forEach((row, y) => {
+        row.forEach((symbol, x) => {
+            if (symbol == 3193) {
+                collisionArray.push(new Boundary((x * 16) + 7, (y * 16) + yOffset, 16, height));
+            }
+        });
+    });
+    Composite.add(world, collisionArray);
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    engine = Engine.create();
+    world = engine.world;
+    player1 = new Player(respawnPosition.x, respawnPosition.y, 16, 28);
+    Composite.add(world, player1);
+    createCollisionBlocks(floorCollision, CollisionBlocks, 16, 32);
+    createCollisionBlocks(platformCollision, platformCollisionBlocks, 1, 2);
+}
+
+function preload() {
+    backgroundImage = loadImage('Sprites/Background/new.png');
+}
+
+const keyState = {
+    d: false,
+    a: false,
+};
+
 window.addEventListener("keydown", (event) => {
-    if (event.key in keys) {
-        keys[event.key] = true;
+    if (event.key in keyState) {
+        keyState[event.key] = true;
     } else if (event.key === 'w' && Math.abs(player1.body.velocity.y) < 0.01) {
         jump();
     }
 });
 
 window.addEventListener("keyup", (event) => {
-    if (event.key in keys) {
-        keys[event.key] = false;
+    if (event.key in keyState) {
+        keyState[event.key] = false;
     }
 });
+
+function applyPlayerForces() {
+    let playerVelocity = { x: 0, y: player1.body.velocity.y };
+
+    if (keyState.a) {
+        left();
+    }
+
+    if (keyState.d) {
+        right();
+    }
+
+    Matter.Body.setVelocity(player1.body, playerVelocity);
+}
+
+function handlePlayerPosition() {
+    if (player1.body.position.x < player1.w / 2) {
+        Matter.Body.setPosition(player1.body, { x: player1.w / 2, y: player1.body.position.y });
+    } else if (player1.body.position.x > width / (zoom - 1) - (player1.w / 2)) {
+        Matter.Body.setPosition(player1.body, { x: width / (zoom - 1) - (player1.w / 2), y: player1.body.position.y });
+    }
+}
+
+function draw() {
+    // Calculate the camera position to follow the player
+    let cameraX = (width / 2) - player1.body.position.x * zoom;
+    let cameraY = (height / 2) - player1.body.position.y * zoom;
+
+    // Set the scale (zoom)
+    scale(zoom);
+
+    // Translate and draw everything accordingly
+    translate(cameraX / zoom, cameraY / zoom);
+
+    // Clear the canvas
+    background(0);
+
+    Engine.update(engine);
+
+    // Draw the background image without applying translation
+    image(backgroundImage, 0, 0);
+
+    // Draw other elements with the camera adjustments applied
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+    }
+
+    if (player1.body.position.y > height * 2) {
+        respawnPlayer();
+    }
+
+    applyPlayerForces();
+    handlePlayerPosition();
+
+    player1.show();
+
+    for (let i = 0; i < CollisionBlocks.length; i++) {
+        CollisionBlocks[i].show();
+    }
+
+    // Reset the translation and scale to their original states
+    translate(-cameraX / zoom, -cameraY / zoom);
+    scale(1 / zoom);
+}
+
 function mouseDragged() {
     boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
 }
 
 function jump() {
-    const force = { x: 0, y: -0.005};
+    const force = { x: 0, y: -0.009 };
+    Matter.Body.applyForce(player1.body, player1.body.position, force);
+}
+
+function right() {
+    const force = { x: 0.004, y: 0 };
+    Matter.Body.applyForce(player1.body, player1.body.position, force);
+}
+
+function left() {
+    const force = { x: -0.004, y: 0 };
     Matter.Body.applyForce(player1.body, player1.body.position, force);
 }
 
 function respawnPlayer() {
     Matter.Body.setPosition(player1.body, respawnPosition);
     Matter.Body.setVelocity(player1.body, { x: 0, y: 0 });
-}
-
-function draw() {
-    image(backgroundImage, 0, 0);
-    Engine.update(engine);
-
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].show();
-    }
-
-    let playerVelocity = { x: 0, y: player1.body.velocity.y };
-
-    if (keys.a) {
-        playerVelocity.x -= 5;
-    }
-
-    if (keys.d) {
-        playerVelocity.x += 5;
-    }
-
-    Matter.Body.setVelocity(player1.body, playerVelocity);
-
-    if (player1.body.position.x < player1.w / 2) {
-        Matter.Body.setPosition(player1.body, { x: player1.w / 2, y: player1.body.position.y });
-    } else if (player1.body.position.x > width - player1.w / 2) {
-        Matter.Body.setPosition(player1.body, { x: width - player1.w / 2, y: player1.body.position.y });
-    }
-
-    ground.show();
-    player1.show();
-    sol.show()
-    for (let i = 0; i < CollisionBlocks.length; i++) {
-        CollisionBlocks[i].show();
-    }
-    if (player1.body.position.y > height + 100) {
-        respawnPlayer();
-    }
 }
