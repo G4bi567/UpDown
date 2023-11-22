39bdb16a38c6d8c0a84496746e29cbf241bb63e9
Gabriel Teixeira <107567915+G4bi567@users.noreply.github.com>
2023-11-22 15:09:48 +0100

    essaie

--- controls.js
+++ controls.js
@@ -1,28 +1,34 @@
+function displayAnimation(animationType, flipHorizontally) {
+    let frame = this.currentFrame % this.frames;
+    let sprite;
 
+    switch (animationType) {
+        case "hit":
+            sprite = this.spriteHit;
+            break;
+        case "jump":
+            sprite = this.spriteJump;
+            break;
+        // Add cases for other animation types
 
-window.addEventListener("keydown", (event) => {
-    if (event.key in keys) {
-        keys[event.key] = true;
-    } else if (event.key === 'w' && Math.abs(player1.body.velocity.y) < 0.01) {
-        jump(player1);
-        jumpSound.play();
-    } else if (event.key === 'ArrowUp' && Math.abs(player2.body.velocity.y) < 0.01 && spawnPlayer2 == true) {
-        jump(player2);
-        jumpSound.play();
-    } else if (event.key === 'g') {
-        // Save the current time when the 'g' key is pressed for Player 2
-        lastKeyPressTimePlayer2 = Date.now();
-    } else if (event.key === 'Escape') {
-        // Toggle the display of the menu
-        const menu = document.getElementById('menu');
-        menu.style.display = (menu.style.display === 'none') ? 'block' : 'none';
-    }
-});
+        default:
+            sprite = this.spriteIdle; // Default to idle sprite
+    };
 
-window.addEventListener("keyup", (event) => {
-    if (event.key in keys) {
-        keys[event.key] = false;
-        movementSound.stop();
-        walkSoundDelay = 0;
-    }
-});
+    push();
+    if (flipHorizontally) {
+        scale(-1, 1);
+    };
+
+    image(
+        sprite,
+        flipHorizontally ? -(this.body.position.x + 50) : this.body.position.x - this.w * this.zoomX / 2,
+        this.body.position.y - this.h * this.zoomY / 2,
+        this.w * this.zoomX,
+        this.h * this.zoomY,
+        frame * this.frameWidth,
+        0,
+        this.frameWidth,
+        this.frameHeight
+    );
+}
